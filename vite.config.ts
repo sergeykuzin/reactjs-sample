661e0bb449dea2e4dfa3cf9a4f9b5972b1752574
Sergey Kuzin <sergeykuzin2008@ya.ru>
2025-05-21 16:59:39 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    base: "/",
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+    },
+});
